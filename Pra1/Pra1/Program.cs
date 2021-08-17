749c5d5f791809fb4941521907b1458adea0132c
tomi1130 <tomiyama891@gmail.com>
2021-08-17 23:34:02 +0900

    例題01

--- Pra1/Pra1/Program.cs
+++ Pra1/Pra1/Program.cs
@@ -6,12 +6,22 @@ namespace Pra1
     {
         static void Main(string[] args)
         {
-            Calc keisan = new Calc();
-            int a = 1, b = 2, c = 3;
-            int ans1 = keisan.Add(a, b);
-            int ans2 = keisan.Add(a, b, c);
+            Vector2D vec = new Vector2D();
+            vec.x = 1.0;
+            vec.y = 2.0;
 
-            Console.WriteLine("{0},{1}", ans1, ans2);
+            Vector2D vec1 = new Vector2D();
+            vec1.x = 3.0;
+            vec1.y = 4.0;
+            vec.Add(vec1);
+            Console.WriteLine("{0}:{1}", vec.x, vec.y);
+            vec.Sub(vec1);
+            Console.WriteLine("{0}:{1}", vec.x, vec.y);
+            vec.Mul(8.0);
+            Console.WriteLine("{0}:{1}", vec.x, vec.y);
+            double result = vec.DotProduct(vec1);
+            Console.WriteLine("{0}", result);
+
         }
     }
 }
---
+++ Pra1/Pra1/Vector2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra1
+{
+    class Vector2D
+    {
+        public double x, y;
+
+        public void Add(Vector2D v)
+        {
+            x += v.x;
+            y += v.y;
+        }
+        public void Sub(Vector2D v)
+        {
+            x -= v.x;
+            y -= v.y;
+        }
+
+        public void Mul(double k)
+        {
+            x *= k;
+            y *= k;
+        }
+
+        public double DotProduct(Vector2D v)
+        {
+            return ((x * v.x) + (y * v.y));
+        }
+    }
+}
