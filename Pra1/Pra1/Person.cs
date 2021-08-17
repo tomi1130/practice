289dcb4ca1eee60bff7d849e9d2c0daaffcff5d7
tm130 <tomiyama891@gmail.com>
2021-08-17 22:36:58 +0900

    練習問題

---
+++ Pra1/Pra1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra1
+{
+    class Person
+    {
+        //名前
+        public string name = "";
+
+        public int age = 0;
+
+        public void ShowAgeAndName() 
+        {
+            Console.WriteLine("名前: {0} 年齢:{1}", name, age);
+        }
+
+        public void SetAgeName(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ Pra1/Pra1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pra1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(); 
+            Person p2 = new Person();
+            p1.name = "田中";
+            p1.age = 20;
+            p1.ShowAgeAndName();
+
+            p2 = new Person();
+            p2.SetAgeName("a", 541);
+            p2.ShowAgeAndName();
+        }
+    }
+}
